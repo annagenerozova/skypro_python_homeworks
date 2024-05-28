2aaf83ad303d43bac0c2a04ff14f123124c1eb01
Anna <anuta2519@yandex.ru>
2024-05-28 19:35:16 +0300

    add second homework

---
+++ lesson3/mailing.py
@@ -0,0 +1,8 @@
+class Mailing:
+
+    def __init__(self,to_address , from_address, cost, track):
+        self.TA = (to_address)
+        self.FA = (from_address)
+        self.cost = int(cost)
+        self.track = str(track)
+
---
+++ lesson3/smartphone.py
@@ -0,0 +1,15 @@
+class Smartphone:
+
+    def __init__(self, stamp , model, number):
+        self.stamp = stamp 
+        self.model = model
+        self.number = number
+
+    # def addStamp(self):
+    #     print(self.stamp)
+    
+    # def addModel(self):
+    #     print(self.model)
+
+    # def addNumber(self):
+    #     print(self.number)
