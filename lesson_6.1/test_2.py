ae9a1dee05612470de14e250dbe8119ce02abef6
Anna <anuta2519@yandex.ru>
2024-06-27 18:23:32 +0300

    add second homework

---
+++ lesson_6.1/test_2.py
@@ -0,0 +1,25 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+def test_calculator(chrome_browser):
+    #chrome_browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    chrome_browser.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    delay_input = chrome_browser.find_element(By.CSS_SELECTOR, '#delay')
+    delay_input.clear()
+    delay_input.send_keys("45")
+
+    chrome_browser.find_element(By.XPATH, '//span[text()="7"]').click()
+    chrome_browser.find_element(By.XPATH, '//span[text()="+"]').click()
+    chrome_browser.find_element(By.XPATH, '//span[text()="8"]').click()
+    chrome_browser.find_element(By.XPATH, '//span[text()="="]').click()
+    
+    WebDriverWait(chrome_browser, 46).until(EC.text_to_be_present_in_element((By.CLASS_NAME, ".screen"), "15"))
+    result_text = chrome_browser.find_element(By.CLASS_NAME , "screen").text
+
+    assert result_text == "15"
+ 
