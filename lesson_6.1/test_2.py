d06b18c69b653641e89a3b8d21791f96bc9623ec
Anna <anuta2519@yandex.ru>
2024-06-28 00:46:45 +0300

    add second homework

--- lesson_6.1/test_2.py
+++ lesson_6.1/test_2.py
@@ -7,8 +7,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 def test_calculator(chrome_browser):
-    #chrome_browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    chrome_browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     chrome_browser.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    chrome_browser.maximize_window()
     delay_input = chrome_browser.find_element(By.CSS_SELECTOR, '#delay')
     delay_input.clear()
     delay_input.send_keys("45")
@@ -18,8 +19,9 @@ def test_calculator(chrome_browser):
     chrome_browser.find_element(By.XPATH, '//span[text()="8"]').click()
     chrome_browser.find_element(By.XPATH, '//span[text()="="]').click()
     
-    WebDriverWait(chrome_browser, 46).until(EC.text_to_be_present_in_element((By.CLASS_NAME, ".screen"), "15"))
+    WebDriverWait(chrome_browser, 46).until(EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), "15"))
     result_text = chrome_browser.find_element(By.CLASS_NAME , "screen").text
+    #assert 'screen' in chrome_browser.find_element(By.ID , '//span[text()="15"]').get_attribute("class")
 
     assert result_text == "15"
  
